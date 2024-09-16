6410e4af4be16ed21816b7ed984086ecefaaf889
Victo <victorcarmona9035@hotmail.com>
2024-09-16 20:10:10 +0100

    Agregar archivos de proyecto.

---
+++ API/Controllers/UserController.cs
@@ -0,0 +1,39 @@
+﻿using DATA;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    public class UserController : BaseApiController
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UserController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetUsers()
+        {
+            var users = await _context.Users.ToListAsync();
+            if(users == null)
+            {
+                return BadRequest("the list is empty");
+            }
+            return Ok(users);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetUser(int id)
+        {
+            var user =await _context.Users.FindAsync(id);
+
+            if (user == null)
+            {
+                return BadRequest("There is no user with that id");
+            }
+            return Ok(user);
+        }
+    }
+}
---
+++ DATA/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MODEL.Entity;
+
+namespace DATA
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
